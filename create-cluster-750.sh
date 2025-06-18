a9bf692e64952d52fa8cea04205131ba32639413
tangenta <tangenta@126.com>
2025-06-18 16:21:23 +0800

    remove others
    Signed-off-by: tangenta <tangenta@126.com>

--- create-cluster-750.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-kubectl create namespace tidb-cluster
-kubectl config set-context --current --namespace="tidb-cluster"
-kubectl -n tidb-cluster apply -f resource/tidb-cluster-v750.yaml
-kubectl -n tidb-cluster wait --for=condition=Ready --timeout=300s tc/tc
-set +euo pipefail
-while true; do kubectl -n tidb-cluster port-forward tc-tidb-0 4000:4000 2>&1 >/dev/null; done &
-while true; do kubectl -n tidb-cluster port-forward tc-tidb-0 10080:10080 2>&1 >/dev/null; done &
--- destroy-cluster.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# kill while true
-ps fjx | grep "[k]ubectl" | tr -s '\t' ' ' | cut -d ' ' -f 2 | xargs -I_ kill _
-# kill kubectl port-forward
-pgrep -lfa kubectl | cut -d ' ' -f1 | xargs -I_ kill _
-kubectl delete tc tc -n tidb-cluster
-kubectl delete pvc -n tidb-cluster -l app.kubernetes.io/instance=tc,app.kubernetes.io/managed-by=tidb-operator
-kubectl get pv -l app.kubernetes.io/namespace=tidb-cluster,app.kubernetes.io/managed-by=tidb-operator,app.kubernetes.io/instance=tc -o name | xargs -I {} kubectl patch {} -p '{"spec":{"persistentVolumeReclaimPolicy":"Delete"}}'
-kubectl delete namespace tidb-cluster
